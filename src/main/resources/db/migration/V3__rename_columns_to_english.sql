0aaa0ed8fd7eda43d81c5e535762bf7b84c2fb3c
DiegoMicali <diego.micali13@gmail.com>
2026-02-16 15:46:04 -0300

    refactor: chance columns names to english

---
+++ src/main/resources/db/migration/V3__rename_columns_to_english.sql
@@ -0,0 +1,4 @@
+ALTER TABLE food_item RENAME COLUMN nome TO name;
+ALTER TABLE food_item RENAME COLUMN categoria TO category;
+ALTER TABLE food_item RENAME COLUMN quantidade TO amount;
+ALTER TABLE food_item RENAME COLUMN validade TO validity;
