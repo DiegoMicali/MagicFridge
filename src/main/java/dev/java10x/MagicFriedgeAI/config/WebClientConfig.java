b22004eeced4d25985cf46df53096d1f347b9086
DiegoMicali <diego.micali13@gmail.com>
2026-02-23 21:27:13 -0300

    feat: add WebClient builder in WebClientConfig

--- src/main/java/dev/java10x/MagicFriedgeAI/config/WebClientConfig.java
+++ src/main/java/dev/java10x/MagicFriedgeAI/config/WebClientConfig.java
@@ -2,15 +2,19 @@ package dev.java10x.MagicFriedgeAI.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class WebClientConfig {
 
-    @Value("${https://api.openai.com/v1/chat/completions}")
+    @Value("${chatgpt.api.url:https://api.openai.com/v1/chat/completions}")
     private String chatGptApiUrl;
 
     @Bean
-    public WebClient webClient (WebClient.Builder builder) {
-        return builder.baseUrl(chatGptApiUrl).build();
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(chatGptApiUrl)
+                .build();
     }
 }
